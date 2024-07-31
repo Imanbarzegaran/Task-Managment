aff46e0f475cb74f45576d51bf88799d2b8446b5
iman barzegaran <133756389+Imanbarzegaran@users.noreply.github.com>
2024-07-31 18:33:06 +0330

    start project

---
+++ Task Managment/Task Managment/Task_ManagmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagmentApp.swift
+//  Task Managment
+//
+//  Created by MacBook Air on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
